d52eed296932acc6e8cc58a638ad71f22da156db
Alex <alex_kolo@gmx.de>
2024-09-16 22:45:19 +0200

    try buttons

--- redirect.py
+++ redirect.py
@@ -1,31 +1,15 @@
 import streamlit as st
 
 # Page title
-st.title("Redirecting...")
+st.title("Redirect Page")
 
 # Specify the URL to redirect to
-redirect_url = "https://nutrify-your-life.streamlit.app/"  # Replace with the desired URL
+redirect_url = "https://nutrify-your-life.streamlit.app/"
 
-# Automatically redirect after 2 seconds
-# st.markdown(
-#     f"""
-#     <meta http-equiv="refresh" content="2; url={redirect_url}">
-#     If you are not redirected automatically, click <a href="{redirect_url}">here</a>.
-# """,
-#     unsafe_allow_html=True,
-# )
+# Information for the user
+st.write(f"The URL of the app has changed to: {redirect_url}")
+st.write(f"[Click here to visit the page]({redirect_url})")
 
-
-# Use JavaScript with a 2-second delay to automatically redirect
-st.markdown(
-    f"""
-    <script type="text/javascript">
-        setTimeout(function() {{
-            window.location.href = "{redirect_url}";
-        }}, 2000);  // 2000 milliseconds = 2 seconds
-    </script>
-
-    You will be redirected in 2 seconds. If you are not redirected automatically, click <a href="{redirect_url}">here</a>.
-""",
-    unsafe_allow_html=True,
-)
+# Button to redirect
+if st.button("Go to Webpage"):
+    st.markdown(f'<a href="{redirect_url}" target="_self">Redirecting...</a>', unsafe_allow_html=True)
